3ea4cbd9125bbb2b057728f76f9b4a97f1ea99ea
Anru Chen <ctx2002@googl.com>
2017-11-09 22:16:48 +1300

    added comments

--- urlProcessing.php
+++ urlProcessing.php
@@ -3,3 +3,4 @@ require_once "bootstrap.php";
 require_once "vendor/autoload.php";
 
 //processing content in url content field, and send to indexing engine for text processing
+//loading content into elastic engine, then do analysis
