d6460aa0d3a47a03945460ac07acc6211455900b
Anru Chen <ctx2002@googl.com>
2017-11-11 12:09:20 +1300

    typo

--- maintenance.php
+++ maintenance.php
@@ -1,5 +1,5 @@
 <?php
 use Softwarewisdom\Crawler\Db\Sqlite3;
 
-shell_exec('supervisor stop urlContent');
+shell_exec('supervisorctl stop urlContent');
 //shell_exec('supervisor start urlContent');
