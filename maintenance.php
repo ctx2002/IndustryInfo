b87c28342f3e2f0d03e67d5c4309fbfb2c4cc753
Anru Chen <ctx2002@googl.com>
2017-11-11 13:45:43 +1300

    added sleep

--- maintenance.php
+++ maintenance.php
@@ -4,7 +4,7 @@ require_once "vendor/autoload.php";
 use Softwarewisdom\Crawler\Db\Sqlite3;
 //this command need to run under sudo mode
 shell_exec('supervisorctl stop urlContent');
-
+sleep(30);//waiting for all sql transaction to been finished.
 global $entityManager;
 $db = new Sqlite3($entityManager);
 $db->run();
