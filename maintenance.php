88dba5d18995dd4415666e3d2015fa386725313a
Anru Chen <ctx2002@googl.com>
2017-11-11 15:17:51 +1300

    delete method

--- maintenance.php
+++ maintenance.php
@@ -4,6 +4,8 @@ require_once "vendor/autoload.php";
 use Softwarewisdom\Crawler\Db\Sqlite3;
 //this command need to run under sudo mode
 shell_exec('supervisorctl stop urlContent');
+
+include "urlDelete.php";
 sleep(30);//waiting for all sql transaction to been finished.
 global $entityManager;
 $db = new Sqlite3($entityManager);
